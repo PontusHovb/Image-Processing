e74b86c4613576e3ec0ae0587dc43bb265eb5efc
pop-pontus <pontus.hovberger@gmail.com>
2023-07-25 19:20:39 +0200

    Include node.js

--- JavaScript/deconvolution.js
+++ JavaScript/deconvolution.js
@@ -3,54 +3,58 @@ const unBlurImageButton = document.getElementById('unBlurImageButton');
 unBlurImageButton.addEventListener('click', unBlurImage);
 
 function unBlurImage() {
-    const width = image[0].length;
-    const height = image.length;
-    const result = [];
-
-    for (let y = 0; y < height; y++) {
-        const row = [];
-        for (let x = 0; x < width; x++) {
-            let realPart = 0;
-            let imagPart = 0;
-            for (let y2 = 0; y2 < height; y2++) {
-                for (let x2 = 0; x2 < width; x2++) {
-                    const pixelValue = image[y2][x2]
-                    const phase = -2 * Math.PI * ((x * x2) / width + (y * y2)  / height);
-                    realPart += pixelValue * Math.cos(phase);
-                    imagPart += pixelValue * Math.sin(phase);
-                }
-            }
-            row.push({re: realPart, imag: imagPart});
-        }
-        result.push(row)
-    }
-    return result;
+    const imageData = ctx.getImageData(0, 0, imagePreview.width, imagePreview.height);
+    const pixels = imageData.data;
+    const width = imageData.width;
+    const height = imageData.height;
+    console.log("PICTURE BEFORE\n")
+    console.log(pixels)
+    const transformedImage = fft2D(pixels)
+    console.log("PICTURE AFTER\n")
+    console.log(transformedImage)
 }
 
-function fourierTransform(imageMatrix) {
-    const width = imageMatrix[0].length;
-    const height = imageMatrix.length;
-    const result = [];
-  
-    for (let v = 0; v < height; v++) {
-      const row = [];
-      for (let u = 0; u < width; u++) {
-        let realPart = 0;
-        let imagPart = 0;
-        for (let y = 0; y < height; y++) {
-          for (let x = 0; x < width; x++) {
-            const pixelValue = imageMatrix[y][x];
-            const phase = -2 * Math.PI * ((u * x) / width + (v * y) / height);
-            realPart += pixelValue * Math.cos(phase);
-            imagPart += pixelValue * Math.sin(phase);
-          }
-        }
-        row.push({ re: realPart, im: imagPart });
+function fft2D(inputMatrix) {
+    const rows = inputMatrix.length;
+    const cols = inputMatrix[0].length;
+
+    // Initialize 2D complex array for input data
+    const input = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+      input[i] = new Array(cols);
+      for (let j = 0; j < cols; j++) {
+        input[i][j] = { re: inputMatrix[i][j], im: 0 };
       }
-      result.push(row);
     }
-  
-    return result;
+
+    // Perform FFT on rows
+    for (let i = 0; i < rows; i++) {
+      input[i] = FFT(input[i]);
+    }
+
+    // Transpose the matrix for column-wise FFT
+    const transposedInput = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+      transposedInput[j] = new Array(rows);
+      for (let i = 0; i < rows; i++) {
+        transposedInput[j][i] = input[i][j];
+      }
+    }
+
+    // Perform FFT on columns
+    for (let j = 0; j < cols; j++) {
+      transposedInput[j] = FFT(transposedInput[j]);
+    }
+
+    // Transpose back to the original orientation
+    const output = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+      output[j] = new Array(rows);
+      for (let i = 0; i < rows; i++) {
+        output[j][i] = transposedInput[i][j];
+      }
+    }
+
+    // Return the transformed 2D array
+    return output;
 }
-  
-  
