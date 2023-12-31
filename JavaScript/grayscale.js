4d18eccb40c27ea6b3c6fb14f3f7b7900b76574a
pop-pontus <pontus.hovberger@gmail.com>
2023-12-31 13:48:55 +0100

    Added function to grayscale image

---
+++ JavaScript/grayscale.js
@@ -0,0 +1,26 @@
+const grayscaleButton = document.getElementById('grayscaleImage');
+const imagePreview = document.getElementById('imagePreview');       
+const ctx = imagePreview.getContext('2d');  
+
+grayscaleButton.addEventListener('click', image_to_grayscale);
+
+function image_to_grayscale ()
+{
+    const imageData = ctx.getImageData(0, 0, imagePreview.width, imagePreview.height);
+    const pixels = imageData.data;
+
+    for (let i = 0; i < pixels.length; i++) {
+        // Extract color of each pixel
+        var red = pixels[i];
+        var green = pixels[i + 1];
+        var blue = pixels[i + 2];
+
+        // Use average as grayscale intensity
+        var avg_color = (red + green + blue) / 3;
+
+        pixels[i] = avg_color;
+        pixels[i + 1] = avg_color;
+        pixels[i + 2] = avg_color;
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
