781d073fb128dc00e16a2ddf228cbd2ec7ed7851
pop-pontus <pontus.hovberger@gmail.com>
2023-12-31 14:34:57 +0100

    Implemented luminosity method for grayscaling

--- JavaScript/grayscale.js
+++ JavaScript/grayscale.js
@@ -4,6 +4,24 @@ const ctx = imagePreview.getContext('2d');
 
 grayscaleButton.addEventListener('click', image_to_grayscale);
 
+const COLOR_WEIGHTS = {
+    red: 0.333,
+    green: 0.333,
+    blue: 0.333
+};
+
+/*
+For average method:
+    red: 0.333,
+    green: 0.333,
+    blue: 0.333
+
+For luminosity method:
+    red: 0.299,
+    green: 0.587,
+    blue: 0.114
+*/
+
 function image_to_grayscale ()
 {
     const imageData = ctx.getImageData(0, 0, imagePreview.width, imagePreview.height);
@@ -16,7 +34,7 @@ function image_to_grayscale ()
         var blue = pixels[i + 2];
 
         // Use average as grayscale intensity
-        var avg_color = red / 3 + green / 3 + blue / 3;
+        var avg_color = red * COLOR_WEIGHTS.red + green * COLOR_WEIGHTS.green + blue * COLOR_WEIGHTS.blue;
 
         pixels[i] = avg_color;
         pixels[i + 1] = avg_color;
