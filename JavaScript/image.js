af1de9ea9db6b2162a9b0f10d0fa0cfe73f8af14
PontusHovb <pontus.hovberger@gmail.com>
2024-07-05 13:32:44 +0200

    New function: resetImage()

--- JavaScript/image.js
+++ JavaScript/image.js
@@ -1,9 +1,14 @@
+const imagePreview = document.getElementById('imagePreview');
+const ctx = imagePreview.getContext('2d');
+let originalImage = null;
 
-const imageInput = document.getElementById('imageInput');           
-const imagePreview = document.getElementById('imagePreview');       
-const ctx = imagePreview.getContext('2d');                          
+const imageInput = document.getElementById('imageInput');
+const resetButton = document.getElementById('resetImage');
+imageInput.addEventListener('change', handleImageSelect);    
+resetButton.addEventListener('click', resetImage);   
 
-imageInput.addEventListener('change', handleImageSelect);               
+const MAX_WIDTH = 400;                                                          // Maximum width for the canvas
+const MAX_HEIGHT = 300;                                                         // Maximum height for the canvas
 
 // Display image once it has been uploaded
 function handleImageSelect(event) {
@@ -11,17 +16,28 @@ function handleImageSelect(event) {
 
     if (file) {
         const reader = new FileReader();
-
         reader.onload = function (event) {
-            const img = new Image();
-            img.onload = function () {
-                imagePreview.width = img.width;
-                imagePreview.height = img.height;
-                ctx.drawImage(img, 0, 0);           // Draw the image on the canvas
-            };
-            img.src = event.target.result;
+            displayImage(event.target.result)
         };
+        
 
         reader.readAsDataURL(file);
+    }
+}
+
+function displayImage(imageSource) {
+        const img = new Image();
+        img.onload = function () {
+            imagePreview.width = img.width;
+            imagePreview.height = img.height;
+            ctx.drawImage(img, 0, 0);                                           // Draw the image on the canvas
+            originalImage = ctx.getImageData(0, 0, img.width, img.height);      // Save image
+        };
+        img.src = imageSource;
+}
+
+function resetImage() {
+    if (originalImage) {
+        ctx.putImageData(originalImage, 0, 0);                                  // Restore the original image data
     }
 }
